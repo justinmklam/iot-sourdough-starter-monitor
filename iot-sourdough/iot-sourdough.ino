69d6d2b79ae9b053700ad36ac4a30e7e5b81860b
Justin Lam <justinmklam@gmail.com>
2020-04-19 15:27:22 -0700

    Initial commit of VL6180X/SSD1306 demo
    From examples repository

---
+++ iot-sourdough/iot-sourdough.ino
@@ -0,0 +1,93 @@
+/* This example shows how to use continuous mode to take
+range measurements with the VL6180X.
+The range readings are in units of mm. */
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Adafruit_VL6180X.h"
+
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+
+Adafruit_SSD1306 display = Adafruit_SSD1306();
+
+#if (SSD1306_LCDHEIGHT != 32)
+ #error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()
+{
+  Serial.begin(9600);
+    
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+  display.display();
+  delay(1000);
+  
+  Serial.println("Adafruit VL6180x test!");
+  if (! vl.begin()) {
+    Serial.println("Failed to find sensor");
+    while (1);
+  }
+  Serial.println("Sensor found!");
+
+  // text display big!
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+}
+
+void loop()
+{
+  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
+
+  Serial.print("Lux: "); Serial.println(lux);
+  
+  uint8_t range = vl.readRange();
+  uint8_t status = vl.readRangeStatus();
+
+  if (status == VL6180X_ERROR_NONE) {
+    Serial.print("Range: "); Serial.println(range);
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print(range);
+    display.print("mm");
+    display.display();
+    Serial.println();
+  } else {
+    display.display();
+    display.clearDisplay();
+    return;
+  }
+
+  // Some error occurred, print it out!
+  
+  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+    Serial.println("System error");
+  }
+  else if (status == VL6180X_ERROR_ECEFAIL) {
+    Serial.println("ECE failure");
+  }
+  else if (status == VL6180X_ERROR_NOCONVERGE) {
+    Serial.println("No convergence");
+  }
+  else if (status == VL6180X_ERROR_RANGEIGNORE) {
+    Serial.println("Ignoring range");
+  }
+  else if (status == VL6180X_ERROR_SNR) {
+    Serial.println("Signal/Noise error");
+  }
+  else if (status == VL6180X_ERROR_RAWUFLOW) {
+    Serial.println("Raw reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RAWOFLOW) {
+    Serial.println("Raw reading overflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEUFLOW) {
+    Serial.println("Range reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEOFLOW) {
+    Serial.println("Range reading overflow");
+  }
+  delay(10);
+}
