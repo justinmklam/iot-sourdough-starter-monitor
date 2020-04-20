4a2c4b609f27bef7d5319ddb9536e02219832703
Justin Lam <justinmklam@gmail.com>
2020-04-20 12:11:59 -0700

    Refactor; remove ESP32 references

--- aws-esp8266-demo/aws-esp8266-demo.ino
+++ aws-esp8266-demo/aws-esp8266-demo.ino
@@ -1,10 +1,4 @@
-#ifdef ESP32
-#include <WiFi.h>
-#elif defined(ESP8266)
 #include <ESP8266WiFi.h>
-#else
-#error Platform not supported
-#endif
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson (use v6.xx)
@@ -15,47 +9,38 @@
 //Enter values in secrets.h ▼
 #include "secrets.h"
 
-#if !(ARDUINOJSON_VERSION_MAJOR == 6 and ARDUINOJSON_VERSION_MINOR >= 7)
-#error "Install ArduinoJson v6.7.0-beta or higher"
-#endif
-
 const int MQTT_PORT = 8883;
 const char MQTT_SUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
 const char MQTT_PUB_TOPIC[] = "$aws/things/" THINGNAME "/shadow/update";
 
-#ifdef USE_SUMMER_TIME_DST
-uint8_t DST = 1;
-#else
-uint8_t DST = 0;
-#endif
-
 WiFiClientSecure net;
-
-#ifdef ESP8266
-BearSSL::X509List cert(cacert);
-BearSSL::X509List client_crt(client_cert);
-BearSSL::PrivateKey key(privkey);
-#endif
-
 PubSubClient client(net);
-
-unsigned long lastMillis = 0;
-time_t now;
-time_t nowish = 1510592825;
 
 void NTPConnect(void)
 {
+  time_t now;
+  time_t nowish = 1510592825;
+  struct tm timeinfo;
+
+#ifdef USE_SUMMER_TIME_DST
+  uint8_t DST = 1;
+#else
+  uint8_t DST = 0;
+#endif
+
   Serial.print("Setting time using SNTP");
   configTime(TIME_ZONE * 3600, DST * 3600, "pool.ntp.org", "time.nist.gov");
   now = time(nullptr);
+
   while (now < nowish)
   {
     delay(500);
     Serial.print(".");
     now = time(nullptr);
   }
+
   Serial.println("done!");
-  struct tm timeinfo;
+
   gmtime_r(&now, &timeinfo);
   Serial.print("Current time: ");
   Serial.print(asctime(&timeinfo));
@@ -66,10 +51,12 @@ void messageReceived(char *topic, byte *payload, unsigned int length)
   Serial.print("Received [");
   Serial.print(topic);
   Serial.print("]: ");
+
   for (int i = 0; i < length; i++)
   {
     Serial.print((char)payload[i]);
   }
+
   Serial.println();
 }
 
@@ -140,43 +127,22 @@ void connectToWiFi(String init_str)
     Serial.println("ok!");
 }
 
-void checkWiFiThenMQTT(void)
-{
-  connectToWiFi("Checking WiFi");
-  connectToMqtt();
-}
-
-unsigned long previousMillis = 0;
-const long interval = 5000;
-
-void checkWiFiThenMQTTNonBlocking(void)
-{
-  connectToWiFi(emptyString);
-  if (millis() - previousMillis >= interval && !client.connected()) {
-    previousMillis = millis();
-    connectToMqtt(true);
-  }
-}
-
-void checkWiFiThenReboot(void)
-{
-  connectToWiFi("Checking WiFi");
-  Serial.print("Rebooting");
-  ESP.restart();
-}
-
 void sendData(void)
 {
   DynamicJsonDocument jsonBuffer(JSON_OBJECT_SIZE(3) + 100);
   JsonObject root = jsonBuffer.to<JsonObject>();
   JsonObject state = root.createNestedObject("state");
   JsonObject state_reported = state.createNestedObject("reported");
+
   state_reported["value"] = random(100);
+
   Serial.printf("Sending  [%s]: ", MQTT_PUB_TOPIC);
   serializeJson(root, Serial);
   Serial.println();
+
   char shadow[measureJson(root) + 1];
   serializeJson(root, shadow, sizeof(shadow));
+
   if (!client.publish(MQTT_PUB_TOPIC, shadow, false))
     pubSubErr(client.state());
 }
@@ -187,25 +153,20 @@ void setup()
   delay(5000);
   Serial.println();
   Serial.println();
-#ifdef ESP32
-  WiFi.setHostname(THINGNAME);
-#else
+
   WiFi.hostname(THINGNAME);
-#endif
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
   connectToWiFi(String("Attempting to connect to SSID: ") + String(ssid));
 
   NTPConnect();
 
-#ifdef ESP32
-  net.setCACert(cacert);
-  net.setCertificate(client_cert);
-  net.setPrivateKey(privkey);
-#else
+  BearSSL::X509List cert(cacert);
+  BearSSL::X509List client_crt(client_cert);
+  BearSSL::PrivateKey key(privkey);
+
   net.setTrustAnchors(&cert);
   net.setClientRSACert(&client_crt, &key);
-#endif
 
   client.setServer(MQTT_HOST, MQTT_PORT);
   client.setCallback(messageReceived);
@@ -215,12 +176,12 @@ void setup()
 
 void loop()
 {
-  now = time(nullptr);
+  static unsigned long lastMillis = 0;
+
   if (!client.connected())
   {
-    checkWiFiThenMQTT();
-    //checkWiFiThenMQTTNonBlocking();
-    //checkWiFiThenReboot();
+    connectToWiFi("Checking WiFi");
+    connectToMqtt();
   }
   else
   {
