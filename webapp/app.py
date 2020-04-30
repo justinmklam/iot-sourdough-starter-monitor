ea33b628513466fd5c02aa5fea3fdcc6a3b78f46
justinmklam <justinmklam@gmail.com>
2020-04-30 08:18:58 -0700

    WIP: Add get data route for pandas dataframe

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import pandas as pd
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+
+@app.route("/get_data", methods=["GET"])
+def get_data():
+    df = pd.DataFrame({
+        "date": ["2020-01-01", "2020-01-02"],
+        "temperature": [1, 2],
+        "humidity": [3, 4],
+        "distance": [5, 6]
+    })
+
+    return df.to_json(orient="records")
 
 
 if __name__ == '__main__':
