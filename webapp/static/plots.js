201d8d1df9ef8190c7aaea75cdc2a635676294db
Justin Lam <justinmklam@gmail.com>
2020-05-01 18:02:44 -0700

    Move plotting javascript to separate file

--- webapp/static/plots.js
+++ webapp/static/plots.js
@@ -1,284 +1,5 @@
 let data = [
     {
-        "year": "1945",
-        "sightings": 6
-    },
-    {
-        "year": "1946",
-        "sightings": 8
-    },
-    {
-        "year": "1947",
-        "sightings": 34
-    },
-    {
-        "year": "1948",
-        "sightings": 8
-    },
-    {
-        "year": "1949",
-        "sightings": 15
-    },
-    {
-        "year": "1950",
-        "sightings": 25
-    },
-    {
-        "year": "1951",
-        "sightings": 20
-    },
-    {
-        "year": "1952",
-        "sightings": 48
-    },
-    {
-        "year": "1953",
-        "sightings": 34
-    },
-    {
-        "year": "1954",
-        "sightings": 50
-    },
-    {
-        "year": "1955",
-        "sightings": 31
-    },
-    {
-        "year": "1956",
-        "sightings": 38
-    },
-    {
-        "year": "1957",
-        "sightings": 67
-    },
-    {
-        "year": "1958",
-        "sightings": 40
-    },
-    {
-        "year": "1959",
-        "sightings": 47
-    },
-    {
-        "year": "1960",
-        "sightings": 64
-    },
-    {
-        "year": "1961",
-        "sightings": 39
-    },
-    {
-        "year": "1962",
-        "sightings": 55
-    },
-    {
-        "year": "1963",
-        "sightings": 75
-    },
-    {
-        "year": "1964",
-        "sightings": 77
-    },
-    {
-        "year": "1965",
-        "sightings": 167
-    },
-    {
-        "year": "1966",
-        "sightings": 169
-    },
-    {
-        "year": "1967",
-        "sightings": 178
-    },
-    {
-        "year": "1968",
-        "sightings": 183
-    },
-    {
-        "year": "1969",
-        "sightings": 138
-    },
-    {
-        "year": "1970",
-        "sightings": 126
-    },
-    {
-        "year": "1971",
-        "sightings": 110
-    },
-    {
-        "year": "1972",
-        "sightings": 146
-    },
-    {
-        "year": "1973",
-        "sightings": 209
-    },
-    {
-        "year": "1974",
-        "sightings": 241
-    },
-    {
-        "year": "1975",
-        "sightings": 279
-    },
-    {
-        "year": "1976",
-        "sightings": 246
-    },
-    {
-        "year": "1977",
-        "sightings": 239
-    },
-    {
-        "year": "1978",
-        "sightings": 301
-    },
-    {
-        "year": "1979",
-        "sightings": 221
-    },
-    {
-        "year": "1980",
-        "sightings": 211
-    },
-    {
-        "year": "1981",
-        "sightings": 146
-    },
-    {
-        "year": "1982",
-        "sightings": 182
-    },
-    {
-        "year": "1983",
-        "sightings": 132
-    },
-    {
-        "year": "1984",
-        "sightings": 172
-    },
-    {
-        "year": "1985",
-        "sightings": 192
-    },
-    {
-        "year": "1986",
-        "sightings": 173
-    },
-    {
-        "year": "1987",
-        "sightings": 193
-    },
-    {
-        "year": "1988",
-        "sightings": 203
-    },
-    {
-        "year": "1989",
-        "sightings": 220
-    },
-    {
-        "year": "1990",
-        "sightings": 217
-    },
-    {
-        "year": "1991",
-        "sightings": 210
-    },
-    {
-        "year": "1992",
-        "sightings": 228
-    },
-    {
-        "year": "1993",
-        "sightings": 285
-    },
-    {
-        "year": "1994",
-        "sightings": 381
-    },
-    {
-        "year": "1995",
-        "sightings": 1336
-    },
-    {
-        "year": "1996",
-        "sightings": 862
-    },
-    {
-        "year": "1997",
-        "sightings": 1248
-    },
-    {
-        "year": "1998",
-        "sightings": 1812
-    },
-    {
-        "year": "1999",
-        "sightings": 2906
-    },
-    {
-        "year": "2000",
-        "sightings": 2780
-    },
-    {
-        "year": "2001",
-        "sightings": 3105
-    },
-    {
-        "year": "2002",
-        "sightings": 3176
-    },
-    {
-        "year": "2003",
-        "sightings": 3896
-    },
-    {
-        "year": "2004",
-        "sightings": 4208
-    },
-    {
-        "year": "2005",
-        "sightings": 3996
-    },
-    {
-        "year": "2006",
-        "sightings": 3590
-    },
-    {
-        "year": "2007",
-        "sightings": 4195
-    },
-    {
-        "year": "2008",
-        "sightings": 4705
-    },
-    {
-        "year": "2009",
-        "sightings": 4297
-    },
-    {
-        "year": "2010",
-        "sightings": 2531
-    }
-]
-//modify away!
-MG.data_graphic({
-    title: "UFO Sightings",
-    description: "Yearly UFO sightings from 1945 to 2010.",
-    data: data,
-    width: 200,
-    height: 200,
-    full_width: true,
-    target: "#ufo-sightings",
-    x_accessor: "year",
-    y_accessor: "sightings",
-});
-
-data = [
-    {
         "date": "2014-02-01",
         "value": 15000000
     },
@@ -682,8 +403,22 @@ data = [
 
 data = MG.convert.date(data, 'date');
 const briefing_1 = MG.data_graphic({
-    title: "Linked Graphic",
+    title: "Distance",
     description: "The two graphics in this section are linked together. A rollover in one causes a rollover in the other.",
+    data: data,
+    linked: true,
+    width: 600,
+    height: 200,
+    full_width: true,
+    right: 40,
+    xax_count: 4,
+    brush: 'x',
+    target: '#briefing-1'
+});
+MG.data_graphic(briefing_1);
+MG.data_graphic({
+    title: "Temperature",
+    description: "Roll over and watch as the graphic to the left triggers.",
     data: data,
     linked: true,
     width: 600,
@@ -693,11 +428,11 @@ const briefing_1 = MG.data_graphic({
     right: 40,
     xax_count: 4,
     brush: 'x',
-    target: '#briefing-1'
+    target: '#briefing-2'
 });
 MG.data_graphic(briefing_1);
 MG.data_graphic({
-    title: "Other Linked Graphic",
+    title: "Humidity",
     description: "Roll over and watch as the graphic to the left triggers.",
     data: data,
     linked: true,
@@ -707,8 +442,6 @@ MG.data_graphic({
     color: 'red',
     right: 40,
     xax_count: 4,
-    zoom_target: briefing_1,
-    shoiwActivePoint: false,
     brush: 'x',
-    target: '#briefing-2'
+    target: '#briefing-3'
 });
