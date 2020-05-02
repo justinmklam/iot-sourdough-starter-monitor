06a5210c5b3bc91270bf4ac84b2c453f6e1a309f
justinmklam <justinmklam@gmail.com>
2020-05-02 13:53:51 -0700

    Add date range picker callback

--- webapp/static/js/daterange.js
+++ webapp/static/js/daterange.js
@@ -1,12 +1,15 @@
-$(function () {
-    $('input[name="datetimes"]').daterangepicker({
-        timePicker: true,
-        timePicker24Hour: true,
-        startDate: moment().startOf('hour').subtract(48, 'hour'),
-        endDate: moment(),
-        maxDate: moment(),
-        locale: {
-            format: 'M/DD HH:mm'
-        }
-    });
+$('#inputDateRange').daterangepicker({
+    timePicker: true,
+    timePicker24Hour: true,
+    startDate: moment().startOf('hour').subtract(48, 'hour'),
+    endDate: moment(),
+    maxDate: moment(),
+    locale: {
+        format: 'MMM D, YYYY h:mm A'
+    }
 });
+
+$('#inputDateRange').on('apply.daterangepicker', function(ev, picker) {
+    console.log(picker.startDate.format("YYYY-MM-DD HH:mm"))
+    console.log(picker.endDate.format("YYYY-MM-DD HH:mm"))
+  });
