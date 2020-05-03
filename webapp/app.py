7b16ea31f1772c4897750692486d0251b238084b
justinmklam <justinmklam@gmail.com>
2020-05-02 21:07:40 -0700

    Refactor construction of data out

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ from random import randint
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return render_template("index.html")
 
@@ -20,20 +20,23 @@ def get_data():
     df = pd.read_json("data.json")
 
     # Randomize horizontal axis
-    df = df.iloc[:randint(10, df.shape[0])]
+    df = df.iloc[: randint(10, df.shape[0])]
     # Randomize data
     df["date"] = df["date"].dt.strftime("%Y-%m-%d")
     df["distance"] = df["distance"] * np.random.randint(0, 100, df.shape[0])
     df["temperature"] = df["temperature"] * np.random.randint(0, 100, df.shape[0])
     df["humidity"] = df["humidity"] * np.random.randint(0, 100, df.shape[0])
 
-    data_out = {
-        "distance": df[["date", "distance"]].rename(columns={"distance": "value"}).to_json(orient="records"),
-        "temperature": df[["date", "temperature"]].rename(columns={"temperature": "value"}).to_json(orient="records"),
-        "humidity": df[["date", "humidity"]].rename(columns={"humidity": "value"}).to_json(orient="records")
-    }
+    data_out = {}
+
+    for col in ["distance", "temperature", "humidity"]:
+        # Output data must have column names "date" and "value" for d3 charts
+        data_out[col] = (
+            df[["date", col]].rename(columns={col: "value"}).to_json(orient="records")
+        )
+
     return jsonify(data_out)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
