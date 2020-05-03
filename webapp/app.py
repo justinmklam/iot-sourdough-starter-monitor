6b1c65151d7d3b43454a9449ef3df2afd2f1c0cd
justinmklam <justinmklam@gmail.com>
2020-05-02 22:40:00 -0700

    Add function to download current data

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import pandas as pd
 import numpy as np
 from random import randint
+from http import HTTPStatus
 
 app = Flask(__name__)
-
+df_global = None
 
 @app.route("/")
 def hello():
@@ -13,6 +14,8 @@ def hello():
 
 @app.route("/get_data", methods=["post"])
 def get_data():
+    global df_global
+
     start_date, start_time = request.form["start"].split()
     end_date, end_time = request.form["end"].split()
     print(start_date, start_time)
@@ -27,6 +30,7 @@ def get_data():
     df["temperature"] = df["temperature"] * np.random.randint(0, 100, df.shape[0])
     df["humidity"] = df["humidity"] * np.random.randint(0, 100, df.shape[0])
 
+    df_global = df
     data_out = {}
 
     for col in ["distance", "temperature", "humidity"]:
@@ -36,6 +40,21 @@ def get_data():
         )
 
     return jsonify(data_out)
+
+
+@app.route("/download_csv", methods=["GET"])
+def download_csv():
+    global df_global
+    df = df_global
+
+    if df is not None:
+        filename = "data.csv"
+        resp = make_response(df.to_csv())
+        resp.headers["Content-Disposition"] = "attachment; filename=" + filename
+        resp.headers["Content-Type"] = "text/csv"
+    else:
+        resp = ('', HTTPStatus.NO_CONTENT)
+    return resp
 
 
 if __name__ == "__main__":
