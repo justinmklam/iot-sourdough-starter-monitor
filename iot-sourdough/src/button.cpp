f923e27351d04c71876648bd5c85391b49e532a4
Justin Lam <justinmklam@gmail.com>
2020-11-22 23:19:50 -0800

    Differentiate short and long button press

--- iot-sourdough/src/button.cpp
+++ iot-sourdough/src/button.cpp
@@ -8,14 +8,27 @@
 PushButton button(BUTTON_PIN);
 
 void initializeButton() {
+    // button.setDebounceDelay(100);
 }
 
 void tButtonCallback() {
     static int buttonState = 0;
+    static int prevButtonState = 0;
+    static long pressTimeStart = millis();
+    static long pressDuration;
 
     buttonState = button.isPressed();
 
-    if (buttonState == HIGH) {
-        Serial.println("Pressed");
+    if (prevButtonState != buttonState) {
+        if (buttonState == HIGH) {
+            Serial.println("Pressed");
+            pressTimeStart = millis();
+        }
+        else {
+            pressDuration = millis() - pressTimeStart;
+            Serial.print(pressDuration);
+            Serial.println(" ms pressed");
+        }
     }
+    prevButtonState = buttonState;
 }
