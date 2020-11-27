4035cd4fcd7b7ff0471c8068a94bbe0d449d26a3
Justin Lam <justinmklam@gmail.com>
2020-11-27 10:53:35 -0800

    Add basic plotting of live data

--- iot-sourdough/src/display.cpp
+++ iot-sourdough/src/display.cpp
@@ -5,7 +5,10 @@
 #include "measurements.h"
 #include "userinput.h"
 
-Adafruit_SSD1306 display = Adafruit_SSD1306();
+#define SSD1306_WIDTH_PX 128
+#define SSD1306_HEIGHT_PX 32
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(SSD1306_WIDTH_PX, SSD1306_HEIGHT_PX);
 
 extern Measurements measurements;
 
@@ -24,12 +27,14 @@ void initializeDisplay() {
 }
 
 void tDisplayCallback() {
-  display.clearDisplay();
-  display.setCursor(0,0);
+  static int prevDisplayState = DISPLAY_STATE_DEFAULT;
 
   switch (getState()) {
     case STATE_CALIBRATION:
+      display.clearDisplay();
+      display.setCursor(0,0);
       display.setTextSize(1);
+
       display.println("Calibration");
       display.print("Jar height: ");
       display.print(measurements.range);
@@ -40,16 +45,41 @@ void tDisplayCallback() {
       // Show the different "dashboards"
       switch (getDisplayState()) {
         case DISPLAY_STATE_DEFAULT:
+          display.clearDisplay();
+          display.setCursor(0,0);
           display.setTextSize(3);
           display.print(measurements.rise_percent);
           display.print("%");
           break;
 
         case DISPLAY_STATE_GRAPH:
-          display.println("graph");
+          static int x0 = 0;
+          static int y0 = SSD1306_HEIGHT_PX;;
+          static int x1 = 0;
+          static int y1 = y0;
+
+          if (prevDisplayState != getDisplayState()) {
+            display.clearDisplay();
+            // Reset coordinates for now until we figure out array plotting
+            x0 = 0;
+            x1 = 0;
+          }
+
+          x1 = x0 + 1;
+          y1 = measurements.rise_height;
+
+          display.writeLine(x0, y0, x1, y1, WHITE);
+          Serial.print(x0);
+          Serial.print(",");
+          Serial.println(y0);
+
+          x0 = x1;
+          y0 = y1;
           break;
 
         case DISPLAY_STATE_ADVANCED:
+          display.clearDisplay();
+          display.setCursor(0,0);
           display.setTextSize(1);
           char adv_row1[128];
           char adv_row2[128];
@@ -72,4 +102,6 @@ void tDisplayCallback() {
       break;
   }
   display.display();
+
+  prevDisplayState = getDisplayState();
 }
