d2e9b63defd81b944c4b02bbf5bd73c99e60e1f0
Justin Lam <justinmklam@gmail.com>
2021-01-20 18:10:24 -0800

    Add second query for getting session data

--- webapp/athena.py
+++ webapp/athena.py
@@ -2,8 +2,10 @@ from pyathena import connect
 from pyathena.pandas.util import as_pandas
 
 
-cursor = connect(s3_staging_dir='s3://levain-monitor-data/staging/',
-                 region_name='us-west-2').cursor()
+connection = connect(s3_staging_dir='s3://levain-monitor-data/staging/',
+                 region_name='us-west-2')
+
+cursor1 = connection.cursor()
 
 query = """
 select sessionid, min(time) as startTime, max(time) as endTime, date_diff('minute', min(time), max(time)) as durationMin
@@ -11,8 +13,17 @@ from levaindatabase.levain_table group by sessionid
 order by startTime desc
 """
 
-cursor.execute(query)
+cursor1.execute(query)
 
-df = as_pandas(cursor)
-print(df.describe())
-print(df.head())
+df1 = as_pandas(cursor1)
+print(df1.head())
+
+
+session_id = 1782426941
+cursor2 = connection.cursor()
+query = f"select * from levaindatabase.levain_table where sessionid = {session_id}"
+
+cursor2.execute(query)
+df2 = as_pandas(cursor2)
+print(df2.head())
+
