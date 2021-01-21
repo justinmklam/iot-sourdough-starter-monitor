37557a73161998243e4f90e6fb55e81a6ea4b7ec
Justin Lam <justinmklam@gmail.com>
2021-01-20 18:05:08 -0800

    Update SQL query to get list of unique sessions

--- webapp/athena.py
+++ webapp/athena.py
@@ -1,11 +1,18 @@
 from pyathena import connect
-from pyathena.pandas_cursor import PandasCursor
+from pyathena.pandas.util import as_pandas
 
 
 cursor = connect(s3_staging_dir='s3://levain-monitor-data/staging/',
-                 region_name='us-west-2',
-                 cursor_class=PandasCursor).cursor()
+                 region_name='us-west-2').cursor()
 
-df = cursor.execute("SELECT * FROM levaindata.levaintable").as_pandas()
+query = """
+select sessionid, min(time) as startTime, max(time) as endTime, date_diff('minute', min(time), max(time)) as durationMin
+from levaindatabase.levain_table group by sessionid
+order by startTime desc
+"""
+
+cursor.execute(query)
+
+df = as_pandas(cursor)
 print(df.describe())
 print(df.head())
